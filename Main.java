26b0a553270c74535c9541cf0831adc68c351b1f
Benjarat Burapajetrak <85210845+geneyeans@users.noreply.github.com>
2022-02-14 14:37:31 +0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,14 @@
+package homework_11;
+public class Main {
+	static int youMethod(int x) {
+		return 5 + x;
+	}
+	
+	public static void main(String[] args) {
+		System.out.println(youMethod(3));	
+	}
+	public static String animal (String word) {
+		System.out.println(word);
+		return word;
+	}
+}
